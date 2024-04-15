6179cd89779dfc9efe51fca955b3b4e023c94c1a
Thibaut Artis <thibaut.artis@varnish-software.com>
2024-04-15 16:51:32 +0200

    vbor: add helper functions

--- lib/libvarnish/vbor.c
+++ lib/libvarnish/vbor.c
@@ -36,6 +36,102 @@
 #include "vas.h"
 #include "vbor.h"
 
+typedef enum
+{
+  VBOR_UINT,
+  VBOR_NEGINT,
+  VBOR_BYTE_STRING,
+  VBOR_TEXT_STRING,
+  VBOR_ARRAY,
+  VBOR_MAP,
+  VBOR_UNKNOWN,
+  VBOR_UNINIT,
+} vbor_major_type_t;
+
+typedef enum
+{
+  VBOR_ARG_5BITS,
+  VBOR_ARG_1BYTE,
+  VBOR_ARG_2BYTES,
+  VBOR_ARG_4BYTES,
+  VBOR_ARG_8BYTES,
+  VBOR_ARG_UNKNOWN,
+} vbor_argument_t;
+
+static size_t
+vbor_length_encoded_size(size_t size)
+{
+  if (size > 0xFFFFFFFF)
+  {
+    return 8;
+  }
+  if (size > 0xFFFF)
+  {
+    return 4;
+  }
+  if (size > 0xFF)
+  {
+    return 2;
+  }
+  if (size > 23)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+static size_t
+vbor_encoded_arg(size_t size)
+{
+  vbor_argument_t arg = VBOR_ARG_5BITS;
+  if (size > 0xFFFFFFFF)
+  {
+    arg = VBOR_ARG_8BYTES;
+  }
+  else if (size > 0xFFFF)
+  {
+    arg = VBOR_ARG_4BYTES;
+  }
+  else if (size > 0xFF)
+  {
+    arg = VBOR_ARG_2BYTES;
+  }
+  else if (size > 23)
+  {
+    arg = VBOR_ARG_1BYTE;
+  }
+  else
+  {
+    return size;
+  }
+  return arg + 0x17;
+}
+
+static void
+vbor_encode_type(vbor_major_type_t type, uint8_t *data)
+{
+  *data = type << 5;
+}
+
+static size_t
+vbor_encode_arg(size_t size, uint8_t *data)
+{
+  size_t size_len = vbor_length_encoded_size(size);
+
+  if (size_len == 0)
+  {
+    *data |= size;
+    return 1;
+  }
+  size_t arg = vbor_encoded_arg(size);
+  *data |= arg << 5;
+  for (size_t i = 0; i < size_len; i++)
+  {
+    data[i] |= (size >> ((size_len - 1 - i) * 8)) & 0xFF;
+  }
+  return 1 + size_len;
+}
+
 struct vbor *
 VBOR_Init(const uint8_t *data, size_t len)
 {
